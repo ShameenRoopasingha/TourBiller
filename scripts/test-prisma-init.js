d1850e17b69c0eb40ec550af32daeca0c7c899b5
Shameen Roopasingha <dinukaroopasingha@gmail.com>
2026-03-21 19:10:47 +0530

    chore: add script to test Prisma client initialization with a connection limit parameter.

---
+++ scripts/test-prisma-init.js
@@ -0,0 +1,7 @@
+const { PrismaClient } = require('@prisma/client');
+try {
+  new PrismaClient({ datasourceUrl: '?connection_limit=5' });
+  console.log('No error on init');
+} catch (e) {
+  console.log('Error on init:', e.message);
+}
