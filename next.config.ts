9db023084fbb56412df3a4eb5f4e8c8e7f2a714a
Shameen Roopasingha <dinukaroopasingha@gmail.com>
2026-09-24 21:58:54 +0530

    chore: add bcrypt to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ['bcrypt'],
   // reactCompiler: true, // Temporarily disabled to fix React DevTools crash
   
   // Security headers to protect against common web vulnerabilities
